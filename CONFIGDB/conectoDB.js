270428413ccd982a0630a38f37935845d4a5882b
hecto123 <hcollazos073@gmail.com>
2025-07-29 19:50:21 -0500

    mi primer trabajo

---
+++ CONFIGDB/conectoDB.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const uri = "mongodb+srv://hcollazos073:bA06375HAWY2NMOa@cluster0.z0q8zlg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+export const conectoDB = async () => {
+    try {
+        await mongoose.connect(uri);
+        console.log("Conexión a la base de datos exitosa");
+    } catch (error) {
+        console.error("Error al conectar a la base de datos:", error);
+    }
+}
